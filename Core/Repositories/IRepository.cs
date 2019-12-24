0d3b8d22621e16552157780f955e2be9dc0c61a4
Sully <jackswilam@gmail.com>
2019-12-24 21:27:07 +0200

    Create IRepository generic interface and Repository generic class that empliments that interface

---
+++ Core/Repositories/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace Core.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        // Read
+        TEntity Get(int id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate);
+
+        // Insert
+        void Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+
+        // Delete
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+    }
+}
---
+++ Persistence/Repositories/Repository.cs
@@ -0,0 +1,62 @@
+﻿using Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext Context;
+        private DbSet<TEntity> _entities;
+
+        public Repository(DbContext context) // context paramter holds the custom context that added in the application
+        {
+            Context = context;
+            _entities = Context.Set<TEntity>(); // entities is a DbSet<TEntity> for eg: Courses, Authors stc
+        }
+
+        public TEntity Get(int id)
+        {
+            return _entities.Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _entities.ToList();
+        }
+
+        public IEnumerable<TEntity> Find(System.Linq.Expressions.Expression<Func<TEntity, bool>> predicate)
+        {
+            return _entities.Where(predicate);
+        }
+
+        public TEntity SingleOrDefault(System.Linq.Expressions.Expression<Func<TEntity, bool>> predicate)
+        {
+            return _entities.SingleOrDefault(predicate);
+        }
+
+        public void Add(TEntity entity)
+        {
+            _entities.Add(entity);
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            _entities.AddRange(entities);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _entities.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            _entities.RemoveRange(entities);
+        }
+    }
+}
