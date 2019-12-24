0f073a2362df64415eb974ddf539691dd22cb104
Sully <jackswilam@gmail.com>
2019-12-24 21:10:02 +0200

    Design database and enable migrations

---
+++ Persistence/EntityConfigurations/CourseConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration;
+using Core.Domain;
+
+namespace Persistence.EntityConfigurations
+{
+    public class CourseConfiguration : EntityTypeConfiguration<Course>
+    {
+        public CourseConfiguration()
+        {
+            Property(c => c.Description)
+               .IsRequired()
+               .HasMaxLength(2000);
+
+            Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            HasRequired(c => c.Author)
+                .WithMany(a => a.Courses)
+                .HasForeignKey(c => c.AuthorId)
+                .WillCascadeOnDelete(false);
+
+            HasRequired(c => c.Cover)
+                .WithRequiredPrincipal(c => c.Course);
+
+            HasMany(c => c.Tags)
+                .WithMany(t => t.Courses)
+                .Map(m =>
+                {
+                    m.ToTable("CourseTags");
+                    m.MapLeftKey("CourseId");
+                    m.MapRightKey("TagId");
+                });
+        }
+    }
+}
---
+++ Persistence/RepositoryContext.cs
@@ -0,0 +1,30 @@
+﻿using Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration;
+using Persistence.EntityConfigurations;
+
+namespace Persistence
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext()
+            : base("name=RepositoryContext")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new CourseConfiguration());
+        }
+    }
+}
