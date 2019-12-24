28a5323f5b25fc54110c0d056fe2a9c56c4f67b4
Sully <jackswilam@gmail.com>
2019-12-24 22:16:35 +0200

    Build UnitOfWork pattern

---
+++ Core/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public interface IUnitOfWork : IDisposable // To implement dispose method.
+    {
+        // Act like DbSets
+        ICourseRepository Courses { get; }
+        IAuthorRepository Authors { get; }
+        int Complete();
+    }
+}
---
+++ Persistence/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+using Core.Repositories;
+using Persistence.Repositories;
+
+namespace Persistence
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly RepositoryContext _context;
+
+        public UnitOfWork(RepositoryContext context)
+        {
+            _context = context;
+            Courses = new CourseRepository(_context);
+            //Authors = new AuthorRepository(_context); // early state.
+        }
+
+        // Act like DbSets.
+        public ICourseRepository Courses { get; private set; } // Or using this way.
+        public IAuthorRepository Authors { get { return new AuthorRepository(_context); } }
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
